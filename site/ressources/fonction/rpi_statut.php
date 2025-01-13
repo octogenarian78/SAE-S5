8d31fa56900f88441e24894530438f7f8c6943db
Enzo GUIGNOLLE <enzoguignolle13@gmail.com>
2025-01-13 15:06:05 +0100

    refactor: Pour les états des RPI on en met que deux

--- site/ressources/fonction/rpi_statut.php
+++ site/ressources/fonction/rpi_statut.php
@@ -16,34 +16,7 @@ function getRpiStatus() {
         exec("ping -c 1 -W 1 $rpiIP[$i] 2>&1", $pingOutput, $pingResult);
 
         if ($pingResult === 0) {
-            // Si le ping réussit, récupère l'utilisation CPU via SSH
-
-            // Commande pour obtenir l'utilisation CPU globale :
-            // 1. 'top -bn1' : Exécute une itération de 'top' en mode batch (sans interface interactive).
-            // 2. '| awk "/%CPU/ {getline; sum += $9} END {print sum}"' :
-            //    - Trouve la ligne avec '%CPU' pour détecter le tableau des processus.
-            //    - 'getline' : Passe à la ligne suivante où commencent les données.
-            //    - 'sum += $9' : Ajoute les valeurs de la 9ème colonne (%CPU de chaque processus).
-            //    - 'END {print sum}' : Affiche la somme totale des utilisations CPU.
-            $cpuUsageCommand = "ssh pi@$rpiIP[$i] 'top -bn1 | awk \"/%CPU/ {getline; sum += \$9} END {print sum}\"'";
-            $cpuUsageOutput = [];
-            exec($cpuUsageCommand, $cpuUsageOutput, $cpuUsageResult);
-            echo "<script>console.log('" . addslashes($cpuUsageResult) . "')</script>";
-            echo "<script>console.log('" . addslashes($cpuUsageOutput[0]) . "')</script>";
-
-            if ($cpuUsageResult === 0 && isset($cpuUsageOutput[0])) {
-                $cpuUsage = floatval($cpuUsageOutput[0]);
-
-                // Si l'utilisation CPU dépasse 80%, on considère que le RPI est "en cours d'utilisation"
-                if ($cpuUsage > 80) {
-                    $statuses[$rpi] = "En cours d'utilisation"; // En jaune
-                } else {
-                    $statuses[$rpi] = "Connecté"; // En vert
-                }
-            } else {
-                // Si la commande SSH ou la récupération CPU échoue
-                $statuses[$rpi] = "Usage incertain"; // En orange
-            }
+            $statuses[$rpi] = "Connecté"; // En vert
         } else {
             // Si le ping échoue, le RPI est considéré comme déconnecté
             $statuses[$rpi] = "Déconnecté"; // En rouge
