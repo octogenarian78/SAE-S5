abf910fcae60d6f1687165eb24a5e154f65f4734
loana <loan.quillet@gmail.com>
2025-01-13 14:44:31 +0100

    fix: Suppression de la partie "2>/dev/null" de la commande

--- site/ressources/fonction/rpi_statut.php
+++ site/ressources/fonction/rpi_statut.php
@@ -25,7 +25,7 @@ function getRpiStatus() {
             //    - 'getline' : Passe à la ligne suivante où commencent les données.
             //    - 'sum += $9' : Ajoute les valeurs de la 9ème colonne (%CPU de chaque processus).
             //    - 'END {print sum}' : Affiche la somme totale des utilisations CPU.
-            $cpuUsageCommand = "ssh pi@$rpi 'top -bn1 | awk \"/%CPU/ {getline; sum += \$9} END {print sum}\" 2>/dev/null'";
+            $cpuUsageCommand = "ssh pi@$rpi 'top -bn1 | awk \"/%CPU/ {getline; sum += \$9} END {print sum}\"'";
             $cpuUsageOutput = [];
             exec($cpuUsageCommand, $cpuUsageOutput, $cpuUsageResult);
 
