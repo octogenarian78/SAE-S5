926aec9a8b52893d38e9ac41af79196ad21814cc
Enzo GUIGNOLLE <enzoguignolle13@gmail.com>
2025-01-20 15:52:27 +0100

    fix: Le bouton graphique avait disparu de la page module

--- site/modules/index.php
+++ site/modules/index.php
@@ -45,8 +45,8 @@ $logoLink = "../";
 if (isset($_SESSION["util_id"])){
     $menuButtons[] = "Accueil";
     $menuLinks[] = "../index.php";
-    $loginButtons = ["Profil"];
-    $loginLinks = ["../profile/index.php"];
+    $loginButtons = ["Profil", "Graphique"];
+    $loginLinks = ["../profile/index.php", "../graphique/"];
 }else{
     $loginButtons = ["Connexion"];
     $loginLinks = ["../signin/index.html"];
